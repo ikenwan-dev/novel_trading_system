c42a771f21a1d74a16aafa47d495fe5eb068829e
ikenwan-dev <theikenwan@gmail.com>
2025-12-16 11:16:38 -0800

    created signal event class and strategy interface

---
+++ src/Strategy/Strategy.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../Events/MarketEvent/MarketEvent.h"
+
+class Strategy {
+public:
+  virtual ~Strategy() = default;
+
+  // Pure virtual function to process market data
+  // To be called by main event loop and will possibly produce signal events
+  virtual void on_market_data(const MarketEvent &event) = 0;
+};
