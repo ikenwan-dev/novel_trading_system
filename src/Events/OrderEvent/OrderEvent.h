53e7fc7c8692035720c175439477b108a1d0d710
theikenwan <theikenwan@gmail.com>
2025-12-26 00:18:18 +0000

    added fill and order events as well as a portfolio header that will be implemented in the following changelists

---
+++ src/Events/OrderEvent/OrderEvent.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Events/Event.h"
+#include <chrono>
+#include <string>
+
+enum class OrderDirection {BUY, SELL};
+
+class OrderEvent : public Event {
+    public:
+    OrderEvent(std::string ticker, std::chrono::system_clock::time_point timestamp, int quantity, OrderDirection direction): ticker_(std::move(ticker)), timestamp_(timestamp), quantity_(quantity), direction_(direction) {}
+
+    EventType get_type() const override { return EventType::Order; }
+
+    const std::string ticker_;
+    const std::chrono::system_clock::time_point timestamp_;
+    const int quantity_;
+    const OrderDirection direction_;
+
+};
