3b9b3b069776f5cb04981374ea36154b12eaf7ec
ikenwan-dev <theikenwan@gmail.com>
2025-12-30 10:58:52 -0800

    handled limit prices

--- src/ExecutionHandlers/SimulatedExecutionHandler/SimulatedExecutionHandler.cpp
+++ src/ExecutionHandlers/SimulatedExecutionHandler/SimulatedExecutionHandler.cpp
@@ -1,17 +1,39 @@
 #include "SimulatedExecutionHandler.h"
 #include "Events/FillEvent/FillEvent.h"
+#include "Events/OrderEvent/OrderEvent.h"
 #include <iostream>
 #include <memory>
 
 void SimulatedExecutionHandler::on_order(const OrderEvent &order_event) {
   auto price_info = data_handler_.get_latest_price_info(order_event.ticker_);
+  bool filled = false;
 
   double simulated_fill_price;
-  if (price_info.has_value()) {
-    simulated_fill_price = price_info.value().open;
-  } else {
+  if (!price_info.has_value()) {
     std::cout << "WARNING: No price info for " << order_event.ticker_
               << ". Halting trade." << std::endl;
+    return;
+  }
+  if (order_event.order_type_ == OrderType::MARKET) {
+    simulated_fill_price = price_info.value().open;
+    filled = true;
+  } else {
+    if (order_event.direction_ == OrderDirection::BUY) {
+      if (order_event.limit_price_.value() >= price_info.value().low) {
+        simulated_fill_price = order_event.limit_price_.value();
+        filled = true;
+      }
+
+    } else {
+      if (order_event.limit_price_.value() <= price_info.value().high) {
+        simulated_fill_price = order_event.limit_price_.value();
+        filled = true;
+      }
+    }
+  }
+
+  if (!filled) {
+    std::cout << "WARNING: Order not filled. Limit price not met." << std::endl;
     return;
   }
 
