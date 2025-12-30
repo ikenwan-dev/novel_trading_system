5e0ce7714aaa8290f95ab7339fe43663de3c45f4
ikenwan-dev <theikenwan@gmail.com>
2025-12-30 10:37:57 -0800

    added todo

--- src/Portfolio/Portfolio.h
+++ src/Portfolio/Portfolio.h
@@ -32,6 +32,7 @@ public:
   }
 
   double get_latest_closing_price(const std::string &ticker) const {
+    // TODO: Handle case where no price info is available
     return data_handler_.get_latest_price_info(ticker).value().close;
   }
 
