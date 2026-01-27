3afab2c4f5d72c489d4e702d2f357865e7bbe28b
ikenwan-dev <theikenwan@gmail.com>
2026-01-26 17:03:14 -0800

    created producer script

--- src/DataProducers/DataProducer.h
+++ src/DataProducers/DataProducer.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "SharedMemory/SharedMemory.h"
+#include <iostream>
 
 class DataProducer {
 public:
   DataProducer(const std::string &shm_name, size_t shm_size)
-      : shm(shm_name, shm_size) {}
+      : shm(shm_name, shm_size, true) {}
   virtual void run() = 0;
+  ~DataProducer() {
+    std::cout << "Unlinking shared memory...\n";
+    shm.unlink();
+  }
 
 protected:
   SharedMemory shm;
---
+++ src/producer.cpp
@@ -0,0 +1,36 @@
+#include "DataProducers/DataBentoProducer/DataBentoProducer.h"
+#include <string>
+#include <vector>
+
+#include <algorithm>
+#include <filesystem>
+#include <iostream>
+
+std::vector<std::string> get_dbn_files(const std::string &directory) {
+  std::vector<std::string> files;
+  const std::string suffix = ".mbo.dbn.zst";
+  for (const auto &entry : std::filesystem::directory_iterator(directory)) {
+    if (entry.is_regular_file()) {
+      std::string path = entry.path().string();
+      if (path.size() >= suffix.size() &&
+          path.compare(path.size() - suffix.size(), suffix.size(), suffix) ==
+              0) {
+        files.push_back(path);
+      }
+    }
+  }
+  std::sort(files.begin(), files.end());
+  return files;
+}
+
+int main() {
+  std::string directory =
+      std::string(PROJECT_ROOT) + "/src/test_data/XNAS-20260120-7W93CD9NGT/";
+  std::vector<std::string> filepaths = get_dbn_files(directory);
+  for (const auto &filepath : filepaths) {
+    std::cout << filepath << '\n';
+  }
+  DataBentoProducer dataBentoProducer("test_shm1", filepaths);
+  dataBentoProducer.run();
+  return 0;
+}
