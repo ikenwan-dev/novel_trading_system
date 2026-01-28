30999faa2b266bd3901cb05afa6781a377680560
ikenwan-dev <theikenwan@gmail.com>
2026-01-27 18:29:50 -0800

    style change

--- src/SharedMemory/SharedRingBuffer.h
+++ src/SharedMemory/SharedRingBuffer.h
@@ -14,43 +14,43 @@ template <typename T, size_t Capacity> class SharedRingBuffer {
                 "Capacity must be power of 2");
 
 public:
-  SharedRingBuffer() : head(0), tail(0) {}
+  SharedRingBuffer() : head_(0), tail_(0) {}
 
   // --- Producer Side ---
   bool push(const T &item) {
-    const size_t current_head = head.load(std::memory_order_relaxed);
+    const size_t current_head = head_.load(std::memory_order_relaxed);
     const size_t next_head = (current_head + 1) & (Capacity - 1);
 
     // Check if buffer is full
-    if (next_head == tail.load(std::memory_order_acquire)) {
+    if (next_head == tail_.load(std::memory_order_acquire)) {
       return false;
     }
 
     buffer[current_head] = item;
     // release ensures 'item' is written before 'head' is updated
-    head.store(next_head, std::memory_order_release);
+    head_.store(next_head, std::memory_order_release);
     return true;
   }
 
   // --- Consumer Side ---
   bool pop(T &item) {
-    const size_t current_tail = tail.load(std::memory_order_relaxed);
+    const size_t current_tail = tail_.load(std::memory_order_relaxed);
 
     // Check if buffer is empty
-    if (current_tail == head.load(std::memory_order_acquire)) {
+    if (current_tail == head_.load(std::memory_order_acquire)) {
       return false;
     }
 
     item = buffer[current_tail];
     // release ensures we are done reading before allowing Producer to overwrite
-    tail.store((current_tail + 1) & (Capacity - 1), std::memory_order_release);
+    tail_.store((current_tail + 1) & (Capacity - 1), std::memory_order_release);
     return true;
   }
 
 private:
   // keep head and tail on separate cache lines to prevent False Sharing
-  alignas(hardware_destructive_interference_size) std::atomic<size_t> head;
-  alignas(hardware_destructive_interference_size) std::atomic<size_t> tail;
+  alignas(hardware_destructive_interference_size) std::atomic<size_t> head_;
+  alignas(hardware_destructive_interference_size) std::atomic<size_t> tail_;
 
   // The data array
   std::array<T, Capacity> buffer;
